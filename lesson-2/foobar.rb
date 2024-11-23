82ca5ee41553076632f715fa1bdead734285971e
Gipnokotik <shootinputin007@gmail.com>
2024-11-24 04:34:34 +0500

    Add fir for foobar method

--- lesson-2/foobar.rb
+++ lesson-2/foobar.rb
@@ -1,16 +1,16 @@
 
-puts "Введите первое число:"
+puts "Введите первое число: "
 num1 = gets.chomp.to_i
 
-puts "Введите второе число:"
+puts "Введите второе число: "
 num2 = gets.chomp.to_i
 
 def foobar(num1, num2)
   if num1 == 20 || num2 == 20
-    num2
+    return num2
   else
-    num1 + num2
+    return num1 + num2
   end
 end
 
-puts "Результат: #{foobar(num1, num2)}"
+puts foobar(num1, num2)
