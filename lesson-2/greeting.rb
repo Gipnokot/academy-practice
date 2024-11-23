cbe2c1ff1aaa50acede09dc2078bd5c1375bf9e0
Gipnokotik <shootinputin007@gmail.com>
2024-11-24 03:35:17 +0500

    Fix greeting.rb

--- lesson-2/greeting.rb
+++ lesson-2/greeting.rb
@@ -9,7 +9,7 @@ def greeting
   if age < 18
     puts "Привет, #{first_name} #{last_name}. Тебе меньше 18 лет, но начать учиться программировать никогда не рано"
   elsif age == 18
-    puts "Вам ровно 18 лет!"
+    puts "Привет #{Frist_name} #{last_name}. Вам ровно 18 лет!"
   else
     "Привет, #{first_name} #{last_name}. Самое время заняться делом!"
   end
