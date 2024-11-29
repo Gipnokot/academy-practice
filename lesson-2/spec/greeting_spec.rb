442ab7cc7167443c5584e233e20ebd9cd97c86f2
Gipnokotik <shootinputin007@gmail.com>
2024-11-29 05:22:46 +0500

    Fix greeting method and test

--- lesson-2/spec/greeting_spec.rb
+++ lesson-2/spec/greeting_spec.rb
@@ -3,17 +3,17 @@
 require_relative '../greeting'
 
 RSpec.describe 'greeting' do
-  it 'returns the correct greeting for age < 18' do
+  it '#returns the correct greeting for age < 18' do
     allow_any_instance_of(Kernel).to receive(:gets).and_return('John', 'Doe', '17')
     expect(greeting).to eq('Привет, John Doe. Тебе меньше 18 лет, но начать учиться программировать никогда не рано')
   end
 
-  it 'returns the correct greeting for age == 18' do
+  it '#returns the correct greeting for age == 18' do
     allow_any_instance_of(Kernel).to receive(:gets).and_return('John', 'Doe', '18')
     expect(greeting).to eq('Привет, John Doe. Вам ровно 18 лет!')
   end
 
-  it 'returns the correct greeting for age > 18' do
+  it '#returns the correct greeting for age > 18' do
     allow_any_instance_of(Kernel).to receive(:gets).and_return('John', 'Doe', '25')
     expect(greeting).to eq('Привет, John Doe. Самое время заняться делом!')
   end
