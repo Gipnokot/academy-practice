fa670d152061a3ab9309123c57d84f06e9872dd4
Gipnokot <shootinputin007@gmail.com>
2025-02-25 02:03:29 +0500

    Add LabReports model and migration

--- lab-reports/db/schema.rb
+++ lab-reports/db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_02_24_172312) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_24_205645) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "lab_reports", force: :cascade do |t|
+    t.string "title", limit: 250, null: false
+    t.text "description"
+    t.string "grade", limit: 2
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_lab_reports_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name", limit: 100, null: false
@@ -22,4 +32,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_24_172312) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
+
+  add_foreign_key "lab_reports", "users"
 end
