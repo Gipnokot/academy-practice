540c03fe5766ba9d70f059d5d3149fdc3d17585d
Gipnokot <shootinputin007@gmail.com>
2025-02-25 23:20:01 +0500

    Add LabReport model

--- lab-reports/app/models/lab_report.rb
+++ lab-reports/app/models/lab_report.rb
@@ -1,3 +1,9 @@
 class LabReport < ApplicationRecord
   belongs_to :user
+
+  GRADES = %w[A B C D E FX F].freeze
+
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :description, length: { maximum: 500 }, allow_blank: true
+  validates :grade, inclusion: { in: GRADES }, allow_blank: true
 end
--- lab-reports/app/models/user.rb
+++ lab-reports/app/models/user.rb
@@ -1,14 +1,8 @@
 class User < ApplicationRecord
   has_many :lab_reports, dependent: :destroy
 
-  validates :first_name,
-            :last_name,
-            presence: true,
-            length: { maximum: 100 }
-
-  validates :email,
-  presence: true,
-  uniqueness: true,
-  lenght: { maximum: 150 },
-  format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :first_name, presence: true, length: { maximum: 100 }
+    validates :last_name, presence: true, length: { maximum: 100 }
+    validates :email, presence: true, uniqueness: true, length: { maximum: 150 },
+                      format: { with: URI::MailTo::EMAIL_REGEXP }
 end
