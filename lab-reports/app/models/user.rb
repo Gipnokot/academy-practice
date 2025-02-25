1664d3ab469fac7047fcafd8c7dd9c1d888f9e39
Gipnokot <shootinputin007@gmail.com>
2025-02-25 12:51:55 +0500

    Add associations and validations to user model

--- lab-reports/app/models/user.rb
+++ lab-reports/app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  has_many :lab_reports, dependent: :destroy
+
+  validates :first_name,
+            :last_name,
+            presence: true,
+            length: { maximum: 100 }
+
+  validates :email,
+  presence: true,
+  uniqueness: true,
+  lenght: { maximum: 150 },
+  format: { with: URI::MailTo::EMAIL_REGEXP }
 end
