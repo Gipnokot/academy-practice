b7a809deaf955323b11c8e1469e2f0bd858b1b93
Gipnokot <shootinputin007@gmail.com>
2025-02-26 23:05:28 +0500

    Create views

--- lab-reports/app/models/lab_report.rb
+++ lab-reports/app/models/lab_report.rb
@@ -7,3 +7,24 @@ class LabReport < ApplicationRecord
   validates :description, length: { maximum: 500 }, allow_blank: true
   validates :grade, inclusion: { in: GRADES }, allow_blank: true
 end
+
+# == Schema Information
+#
+# Table name: lab_reports
+#
+#  id          :bigint           not null, primary key
+#  description :text
+#  grade       :string(2)
+#  title       :string(250)      not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :bigint           not null
+#
+# Indexes
+#
+#  index_lab_reports_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
--- lab-reports/config/environment.rb
+++ lab-reports/config/environment.rb
@@ -3,3 +3,10 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Rails.application.config.after_initialize do
+  Bullet.enable = true
+  Bullet.alert = true
+  Bullet.bullet_logger = true
+  Bullet.console = true
+end
